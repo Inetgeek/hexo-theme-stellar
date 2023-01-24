d8140e58ed19a8a2b10c0c98da3ae7865e94c6c1
inetgeek <ranshens@foxmail.com>
2023-01-25 06:17:20 +0800

    fix: network access failed

--- scripts/tags/lib/ghcard.js
+++ scripts/tags/lib/ghcard.js
@@ -24,10 +24,15 @@ module.exports = ctx => function(args) {
   if (path.includes('/')) {
     // is repo
     const ps = path.split('/')
-    url += 'https://github-readme-stats.xaoxuu.com/api/pin/?username=' + ps[0] + '&repo=' + ps[1]
+    url += 'https://github-readme-stats.vercel.app/api/pin/?username=' + ps[0] + '&repo=' + ps[1]
+    //2023-01-25
+    //url += 'https://github-readme-stats.xaoxuu.com/api/pin/?username=' + ps[0] + '&repo=' + ps[1]
   } else {
     // is user
-    url += 'https://github-readme-stats.xaoxuu.com/api/?username=' + path
+    
+    url += 'https://api.allorigins.win/raw?url=https://github-readme-stats.vercel.app/api/?username=' + path
+    //2023-01-25
+    //url += 'https://github-readme-stats.xaoxuu.com/api/?username=' + path
   }
   url += '&' + ctx.args.joinURLParams(args, params)
   if (!url.includes('&show_owner=')) {
